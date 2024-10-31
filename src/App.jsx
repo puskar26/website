de29cd20e12f9da07384705ba47b3481f8007861
Pushkar Niraula <puskarniraula01@gmail.com>
2024-10-31 22:59:39 +0545

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-3xl">Hello I will create my website</div>
+    </>
+  );
+}
+
+export default App;
